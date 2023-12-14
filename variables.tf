c6032e6dd1420b6ba3ed138b82e6735037ddf32e
Saha <Jayanta.Saha@shell.com>
2023-12-14 17:59:37 +0530

    updated terraform var definiton

--- variables.tf
+++ variables.tf
@@ -3,22 +3,12 @@ variable "client_id"{
     default     = "default-client-id"
 }
 
-variable "client_secret"{
-    description = "Azure client secret"
-    default     = "default-client-secret"
+variable "workspace_name"{
+    description = "Default workspace name"
+    default     = "default-workspace-name"
 }
 
 variable "subscription_id"{
     description = "Azure subscription ID"
     default     = "default-subscription-id"
-}
-
-variable "tenant_id"{
-    description = "Azure tenant id"
-    default     = "default-tenant-id"
-}
-
-variable "workspace_name"{
-    description = "Default workspace name"
-    default     = "default-workspace-name"
 }
